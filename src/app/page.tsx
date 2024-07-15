2279890dbbfdaaa904c40df3544b46581f48e375
Ayushkalathiya <ayushkalathiya50@gmail.com>
2024-07-15 22:21:27 +0530

    feat:small change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,27 +3,38 @@ import Navbar from "@/components/Navbar"
 import { db } from "@/db/db"
 import Loading from "./loading"
 import Image from "next/image"
+import Link from "next/link"
 
 export default async function Home() {
   return (
-     <main className="flex flex-col">
+    <main className="flex flex-col">
       <Navbar />
-    
-        <MaxWidthWrapper>
-          <div className="flex flex-col-reverse md:flex-row justify-center items-center m-8 gap-8 md:gap-16">
-            <div className="p-4 mt-4 text-center md:text-left">
-              <p className="text-xl  md:text-2xl font-bold">
-                Track your spending, unleash your saving!
-                <br />
-                Transform expenses into financial freedom!
-              </p>
-            </div>
-            <div className="pl-0 mt-16 md:pl-10">
-              <Image src="/main_page.png" width="600" height="600" alt="nothing" />
-            </div>
+
+      <MaxWidthWrapper>
+        <div className="m-8 flex flex-col-reverse items-center justify-center gap-8 md:flex-row md:gap-16">
+          <div className="mt-4 p-4 text-center md:text-left">
+            <p className="text-xl font-bold mb-5 md:text-2xl">
+              Track your spending, unleash your saving!
+              <br />
+              Transform expenses into financial freedom!
+            </p>
+            <Link
+              href="/signup"
+              className="hover:bg-primary-dark  rounded-lg bg-primary px-6 py-2 text-white transition-colors"
+            >
+              Start Your Journey
+            </Link>
           </div>
-        </MaxWidthWrapper>
-      </main>
- 
+          <div className="mt-16 pl-0 md:pl-10">
+            <Image
+              src="/main_page.png"
+              width="600"
+              height="600"
+              alt="nothing"
+            />
+          </div>
+        </div>
+      </MaxWidthWrapper>
+    </main>
   )
 }
