ec4e39efea8a47a47f12da6b238a406bf179abbe
Dhruv Kotadiya <22ce054@charusat.edu.in>
2024-08-05 09:39:03 +0530

    Minor Changes

--- src/app/api/allData/route.ts
+++ src/app/api/allData/route.ts
@@ -1,34 +1,33 @@
-import { db } from "@/lib/db";
-import { NextRequest, NextResponse } from "next/server";
-
+import { db } from "@/lib/db"
+import { NextRequest, NextResponse } from "next/server"
 
 export async function GET(req: NextRequest) {
   try {
-    const searchParams = req.nextUrl.searchParams;
-    const userId = searchParams.get("userId");
-    const startDate = searchParams.get("startDate");
-    const endDate = searchParams.get("endDate");
+    const searchParams = req.nextUrl.searchParams
+    const userId = searchParams.get("userId")
+    const startDate = searchParams.get("startDate")
+    const endDate = searchParams.get("endDate")
 
     if (!userId || typeof userId !== "string") {
-      console.log("No user ID provided");
+      console.log("No user ID provided")
       return NextResponse.json(
         { error: "User ID Not Provided" },
         { status: 400 }
-      );
+      )
     }
 
-    let whereClause: any = { userId: userId };
+    let whereClause: any = { userId: userId }
 
     if (startDate && endDate) {
-      const start = new Date(startDate);
-      start.setHours(0, 0, 0, 0);
-      const end = new Date(endDate);
-      end.setHours(23, 59, 59, 999);
+      const start = new Date(startDate)
+      start.setHours(0, 0, 0, 0)
+      const end = new Date(endDate)
+      end.setHours(23, 59, 59, 999)
 
       whereClause.date = {
         gte: start,
         lte: end,
-      };
+      }
     }
 
     const [expense, income] = await Promise.all([
@@ -37,29 +36,25 @@ export async function GET(req: NextRequest) {
       }),
       db.income.findMany({
         where: whereClause,
-      })
-    ]);
+      }),
+    ])
 
-    console.log(whereClause);
+    console.log(whereClause)
 
     // console.log("Income data");
     // console.log(income);
     // console.log("Expense data");
     // console.log(expense);
-    
-    
-    
 
-    return NextResponse.json({ expense, income });
+    return NextResponse.json({ expense, income })
   } catch (error) {
-    console.error("Error in GET function:", error);
+    console.error("Error in GET function:", error)
     return NextResponse.json(
       { error: "Internal Server Error" },
       { status: 500 }
-    );
+    )
   }
 }
-
 
 // export async function GET(req: NextRequest) {
 //   try {
