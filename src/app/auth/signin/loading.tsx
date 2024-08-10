16e18b53c97dc0aee9e753009c19ea0ca49d1231
Dhruv Kotadiya <22ce054@charusat.edu.in>
2024-08-10 18:26:36 +0530

    Chnaged image name at some pages

--- src/app/auth/signin/loading.tsx
+++ src/app/auth/signin/loading.tsx
@@ -4,7 +4,7 @@ export default function Loading() {
   return (
     <div className="flex h-screen w-screen items-center justify-center bg-gray-100 dark:bg-gray-900">
       <div className="flex flex-col items-center">
-        <Image src="/SpendWise-5.png" width="300" height="300" alt="loading" />
+        <Image src="/SpendWIse-5.png" width="300" height="300" alt="loading" />
         <p className="mt-4 text-5xl font-bold text-gray-800 dark:text-gray-200">
           Spend<span className="font-bold text-primary">Wise</span>
         </p>
