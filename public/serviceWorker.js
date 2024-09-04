e07a6f6be6aab4052c1baf7696a7971f6c918778
Dhruv Kotadiya <22ce054@charusat.edu.in>
2024-09-04 15:05:20 +0530

    Updated Service Worker

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -3,7 +3,6 @@ self.addEventListener("push", function (event) {
   console.log("Push event received")
   if (event.data) {
     const data = event.data.json()
-    console.log("Received push data:", data)
     const { title, body, type, data: customData } = data
 
     const notificationOptions = {
@@ -15,27 +14,19 @@ self.addEventListener("push", function (event) {
       renotify: true,
     }
 
-    console.log("Notification options:", notificationOptions)
-
     event.waitUntil(
       self.registration
         .showNotification(title, notificationOptions)
         .then(() => console.log("Notification shown successfully"))
         .catch((error) => console.error("Error showing notification:", error))
     )
-  } else {
-    console.log("Push event received but no data")
   }
 })
 
 // Service Worker Notification Click Event Handler
 self.addEventListener("notificationclick", function (event) {
-  console.log("Notification clicked")
   const notification = event.notification
   const { type, url } = notification.data
-
-  console.log("Notification type:", type)
-  console.log("Notification data:", notification.data)
 
   event.notification.close()
 
@@ -47,20 +38,16 @@ self.addEventListener("notificationclick", function (event) {
           return windowClient.focus()
         }
       })
-      .catch((error) => {
-        console.error("Error opening window:", error)
-      })
+      .catch((error) => {})
   )
 })
 
 // Service Worker Installation Event Handler
 self.addEventListener("install", function (event) {
-  console.log("Service Worker installed")
   event.waitUntil(self.skipWaiting())
 })
 
 // Service Worker Activation Event Handler
 self.addEventListener("activate", function (event) {
-  console.log("Service Worker activated")
   event.waitUntil(self.clients.claim())
 })
