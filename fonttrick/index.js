3052fed9bf78097bb0ec0d6f907647e43cba708f
Dhruv Kotadiya <22ce054@charusat.edu.in>
2024-09-18 18:19:13 +0530

    Tried Again

--- fonttrick/index.js
+++ fonttrick/index.js
@@ -1,25 +1,41 @@
-module.exports = function fonttrick() {
-  const fs = require("fs")
-  const path = require("path")
-  const RobotoR = require.resolve("./fonts/Poppins-Regular.ttf")
-  const { COPYFILE_EXCL } = fs.constants
-  const { COPYFILE_FICLONE } = fs.constants
+const fs = require("fs")
+const path = require("path")
 
-  //const pathToRoboto = path.join(process.cwd(), 'node_modules/fonttrick/Roboto-Regular.ttf')
+module.exports = function fonttrick() {
+  const tempFontPath = path.join("/tmp", "Roboto-Regular.ttf")
+  const sourceFontPath = require.resolve("./fonts/Poppins-Regular.ttf")
+
+  console.log(`Source font path: ${sourceFontPath}`)
+  console.log(`Temporary font path: ${tempFontPath}`)
 
   try {
-    if (fs.existsSync("/tmp/Roboto-Regular.ttf")) {
-      console.log("Roboto lives in tmp!!!!")
-    } else {
+    if (!fs.existsSync(tempFontPath)) {
+      console.log("Copying font to /tmp directory...")
+
+      // Check if sourceFontPath is a string
+      if (typeof sourceFontPath !== "string") {
+        throw new TypeError(
+          `Expected sourceFontPath to be a string, got ${typeof sourceFontPath}`
+        )
+      }
+
+      // Ensure the source file exists
+      if (!fs.existsSync(sourceFontPath)) {
+        throw new Error(`Source font file does not exist: ${sourceFontPath}`)
+      }
+
       fs.copyFileSync(
-        RobotoR,
-        "/tmp/Roboto-Regular.ttf",
-        COPYFILE_FICLONE | COPYFILE_EXCL
+        sourceFontPath,
+        tempFontPath,
+        fs.constants.COPYFILE_FICLONE | fs.constants.COPYFILE_EXCL
       )
+      console.log("Font copied successfully!")
+    } else {
+      console.log("Font already exists in /tmp directory.")
     }
   } catch (err) {
-    console.error(err)
+    console.error("Error while handling the font file:", err)
   }
 
-  return "/tmp/Roboto-Regular.ttf"
+  return tempFontPath
 }
