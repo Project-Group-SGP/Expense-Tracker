a92814c768da334ac2d1dcb34bfb1729bd1a851d
Ayushkalathiya <ayushkalathiya50@gmail.com>
2024-09-19 20:04:21 +0530

    vercel : Web Analytics Added

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,5 +1,6 @@
 import RegisterServiceWorker from "@/components/RegisterServiceWorker"
 import Navbar from "./dashboard/_components/Navbar"
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({
   children,
@@ -10,6 +11,7 @@ export default function RootLayout({
     <>
       <Navbar />
       {children}
+      <Analytics />
       <RegisterServiceWorker />
     </>
   )
