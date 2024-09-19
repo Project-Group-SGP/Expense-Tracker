1dcb6100e1633dda4f0c163ee78ee9429718a87c
Ayushkalathiya <ayushkalathiya50@gmail.com>
2024-09-19 20:10:48 +0530

    Vercel : Added Speed Insights

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,6 +1,7 @@
 import RegisterServiceWorker from "@/components/RegisterServiceWorker"
 import Navbar from "./dashboard/_components/Navbar"
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export default function RootLayout({
   children,
@@ -12,6 +13,7 @@ export default function RootLayout({
       <Navbar />
       {children}
       <Analytics />
+      <SpeedInsights/>
       <RegisterServiceWorker />
     </>
   )
