e7515310352fe15505d8827699bb8d350f4eb5bf
Dhruv Kotadiya <22ce054@charusat.edu.in>
2024-09-20 14:41:57 +0530

    Removed Unused Dependencies

--- next.config.mjs
+++ next.config.mjs
@@ -7,16 +7,6 @@ const nextConfig = {
       fullUrl: true,
     },
   },
-  webpack: (config, { isServer }) => {
-    config.module.rules.push({
-      test: /\.(woff|woff2|eot|ttf|otf)$/i,
-      type: "asset/resource",
-      generator: {
-        filename: "static/fonts/[name][ext]",
-      },
-    })
-    return config
-  },
 }
 
 const pwaConfig = {
