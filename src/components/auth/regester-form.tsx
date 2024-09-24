7d954844eb5934c9cc66486c31a1011b4c40dfbc
SARTHAK <mayanisarthak@gmail.com>
2024-09-24 14:05:55 +0530

    register from error

--- src/components/auth/regester-form.tsx
+++ src/components/auth/regester-form.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React, { useCallback, useMemo, useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
